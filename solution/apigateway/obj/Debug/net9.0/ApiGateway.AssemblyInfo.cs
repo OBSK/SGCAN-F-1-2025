5910cb25ebe49b7131e1c170e894c1559e519470
Jorge Arturo Huima Ruiz <jarturo_huima@hotmail.com>
2025-02-04 12:17:40 -0500

    [Finalizado] Refactor Docker Compose and react files

--- solution/apigateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
+++ solution/apigateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531d547ba71a58dd362dd9150fd0d8adf9f08501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0074a6e931236dca5d64f0810926da2c3a127c87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
